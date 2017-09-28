2f04ba11509962d7279db32d1fd3679a3067ab8a
DavidGomez27 <mmcsgomez16@gmail.com>
2017-09-28 14:43:59 -0400

    second commit

--- Week1_Day4/Program.cs
+++ Week1_Day4/Program.cs
@@ -10,6 +10,58 @@ namespace Week1_Day4
     {
         static void Main(string[] args)
         {
+
+            //A string is a sequence of characters inside "double quotes"
+
+            //Console.WriteLine("This is a tab \t.");
+            //Console.WriteLine("This is a \"quote\".");
+            //Console.WriteLine("This is a backslash \\");
+            //Console.WriteLine("This is a \n new line.");
+            //Console.WriteLine("This is a bell \a");
+
+
+            //New example below
+
+
+            //string firstName = "Daniel";
+            //string dogName = "Gus";
+
+            ////Console.WriteLine("My name is {0} and my dog is {1}.", firstName, dogName);
+
+
+            //string bestFriends = firstName + " " + dogName;
+            //Console.WriteLine(bestFriends);
+
+            //New Example below for ".Legnth" which numbers the characters
+
+            //string firstName = "Daniel";
+            //string bestFriend = firstName + "Gus";
+            //Console.WriteLine(firstName.Length);
+            //Console.WriteLine(bestFriend.Length);
+
+            //New example below for "Last Index of"
+
+            ////
+            //// The string we are searching.
+            //string value = "Dot Net Perls";
+            ////
+            //// Find the last occurrence of N.
+            //int index1 = value.LastIndexOf('N');
+            //if (index1 != -1)
+            //{
+            //    Console.WriteLine(index1);
+            //    Console.WriteLine(value.Substring(index1));
+            //}
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
